051702b1fbeb49ed7d8123eea31422ad614ce645
riyanaditiya <riyanaditiyasaputra@gmail.com>
2026-07-17 07:17:10 +0700

    feat: added home page

---
+++ src/data/products.js
@@ -0,0 +1,55 @@
+const products = [
+    {
+        id: 1,
+        name: "Milk and Honey",
+        price: 150.000,
+        image: "https://images.unsplash.com/photo-1544947950-fa07a98d237f?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni magnam neque eum voluptatibus iste voluptatem porro eius officiis asperiores natus nostrum quam, tempora esse iure in minus debitis dolore"
+    },
+    {
+        id: 2,
+        name: "The Golden Age",
+        price: 120.000,
+        image: "https://images.unsplash.com/photo-1659938306988-a3d63d5e5cef?q=80&w=435&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni magnam neque eum voluptatibus iste voluptatem porro eius officiis asperiores natus nostrum quam, tempora esse iure in minus debitis dolore"
+    },
+    {
+        id: 3,
+        name: "Coco",
+        price: 99.000,
+        image: "https://images.unsplash.com/photo-1659938307000-f6dc583ab2f3?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni magnam neque eum voluptatibus iste voluptatem porro eius officiis asperiores natus nostrum quam, tempora esse iure in minus debitis dolore"
+    },
+    {
+        id: 4,
+        name: "Last Waltz In Vienna",
+        price: 150.000,
+        image: "https://images.unsplash.com/photo-1542639130-b6a81c306c3e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni magnam neque eum voluptatibus iste voluptatem porro eius officiis asperiores natus nostrum quam, tempora esse iure in minus debitis dolore"
+    },
+    {
+        id: 5,
+        name: "Coffee Dictionary",
+        price: 130.000,
+        image: "https://images.unsplash.com/photo-1729693862649-c457544c9c69?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni magnam neque eum voluptatibus iste voluptatem porro eius officiis asperiores natus nostrum quam, tempora esse iure in minus debitis dolore"
+    },
+    {
+        id: 6,
+        name: "The Silver Chair",
+        price: 200.000,
+        image: "https://images.unsplash.com/photo-1555944456-11933ff38379?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni magnam neque eum voluptatibus iste voluptatem porro eius officiis asperiores natus nostrum quam, tempora esse iure in minus debitis dolore"
+    },
+    {
+        id: 7,
+        name: "Modern Sanat",
+        price: 50.000,
+        image: "https://images.unsplash.com/photo-1744995380732-5e24361b7339?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni magnam neque eum voluptatibus iste voluptatem porro eius officiis asperiores natus nostrum quam, tempora esse iure in minus debitis dolore"
+    }
+];
+
+export function getProducts(){
+    return products;
+}
